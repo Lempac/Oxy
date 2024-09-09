af6a5182f90b8efc1f6bfead4f1df7330a84a8e6
Lempac <brunis.biksis@gmail.com>
2024-09-09 20:38:00 +0300

    Added Relation for models
    Signed-off-by: Lempac <brunis.biksis@gmail.com>

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Note extends Model
 {
@@ -13,4 +14,9 @@ class Note extends Model
         'title',
         'text',
     ];
+
+    public function board() : BelongsTo
+    {
+        return $this->belongsTo(Board::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -43,5 +45,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function servers(): MorphToMany
+    {
+        return $this->morphToMany(Server::class, 'servers');
+    }
+
+    public function messages(): HasMany
+    {
+        return $this->hasMany(Message::class);
     }
 }
