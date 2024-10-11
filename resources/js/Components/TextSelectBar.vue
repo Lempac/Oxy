26f04f34e48e51a28823483ca4d3314ac2cae543
M7PAX <niksup10@gmail.com>
2024-10-11 20:26:36 +0300

    small fixes

--- resources/js/Components/TextSelectBar.vue
+++ resources/js/Components/TextSelectBar.vue
@@ -6,11 +6,9 @@ addIcons(BiChatText, RiChatVoiceLine, MdViewkanbanOutlined);
 </script>
 
 <template>
-  <div class="navbar bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700" v-if="$page.props.channels && $page.props.channels.length > 0">
-    <div class="navbar-center hidden lg:flex">
-      <button v-for="channel in $page.props.channels" :key="channel.id" class="menu menu-horizontal px-1">
+  <div class="navbar bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700 justify-around" v-if="$page.props.channels && $page.props.channels.length > 0">
+      <button v-for="channel in $page.props.channels" :key="channel.id" class="btn btn-wide">
         {{ channel.name }}
       </button>
-    </div>
   </div>
 </template>
--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,21 +1,26 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+import { usePage } from '@inertiajs/vue3';
 import ServerSelectBar from "@/Components/ServerSelectBar.vue";
 import ChannelSelectBar from "@/Components/ChannelSelectBar.vue";
 import TextSelectBar from "@/Components/TextSelectBar.vue";
 
+const page = usePage();
+const isHome = computed(() => page.url.startsWith('/home'));
+
 </script>
 
 <template>
-    <div>
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            <ServerSelectBar></ServerSelectBar>
-            <ChannelSelectBar></ChannelSelectBar>
-            <TextSelectBar></TextSelectBar>
+    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <ServerSelectBar />
 
-            <!-- Page Content -->
-            <main>
-                <slot/>
-            </main>
-        </div>
+        <header v-if="isHome">
+            <ChannelSelectBar />
+            <TextSelectBar />
+        </header>
+
+        <main>
+            <slot />
+        </main>
     </div>
 </template>
