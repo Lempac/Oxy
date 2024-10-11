0bdd8266f9a42970b42034f6c215ac78401f389f
Lempac <brunis.biksis@gmail.com>
2024-10-11 12:17:40 +0300

    Remove header from Home.vue

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -7,13 +7,6 @@ import {Head, usePage} from '@inertiajs/vue3';
     <Head title="Home" />
 
     <AuthenticatedLayout>
-<<<<<<< HEAD:resources/js/Pages/Dashboard.vue
-=======
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Home</h2>
-        </template>
-
->>>>>>> main:resources/js/Pages/Home.vue
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
