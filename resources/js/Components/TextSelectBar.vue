4348e368a965a81750bf427208a184bcf3d3ebb9
M7PAX <niksup10@gmail.com>
2024-10-16 21:20:51 +0300

    merge fix 3

--- resources/js/Components/TextSelectBar.vue
+++ resources/js/Components/TextSelectBar.vue
@@ -9,7 +9,7 @@ const serverId = selected_server?.id;
 </script>
 
 <template>
-    <div class="navbar bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700 justify-around" v-if="$page.props.channels.length > 0">
+    <div class="navbar bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700 justify-around" v-if="$page.props.channels && $page.props.channels.length > 0">
         <Link v-for="channel in $page.props.channels" :key="channel.id" :href="route('home.channel', {server : serverId, channel : channel.id} )">
             <button class="btn btn-wide">
                 {{ channel.name }}
