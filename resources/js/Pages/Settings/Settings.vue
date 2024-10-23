24bf11516c9897dadd382886418f20ab0f0bb7b8
Lempac <brunis.biksis@gmail.com>
2024-10-23 22:58:50 +0300

    Work in progress

---
+++ resources/js/Pages/Settings/Settings.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+    import {ref} from "vue";
+    import Server from "@/Pages/Settings/Server.vue";
+    import Role from "@/Pages/Settings/Role.vue";
+
+    const serverSettingsModal = ref<HTMLDialogElement>();
+    const activeTab = ref<"server" | "role">("server");
+
+    defineExpose({serverSettingsModal})
+</script>
+
+<template>
+    <dialog class="modal" ref="serverSettingsModal">
+        <div class="modal-action">
+            <button>
+
+            </button>
+        </div>
+<!--        <Server v-if="activeTab === 'server'"/>-->
+<!--        <Role v-if="activeTab === 'role'"/>-->
+    </dialog>
+</template>
+
+<style scoped>
+
+</style>
