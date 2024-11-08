4f16eea8bb3f9d8455b06ccf21ea29dd44671175
Edgrrr <13DPEFreimanis@rvt.lv>
2024-11-08 09:37:04 +0200

    Small tweek

--- resources/js/Components/ServerSelectBar.vue
+++ resources/js/Components/ServerSelectBar.vue
@@ -8,7 +8,7 @@ import {addIcons} from "oh-vue-icons";
 import {OiPlus} from "oh-vue-icons/icons";
 addIcons(OiPlus);
 
-const isHomePage = computed(() => usePage().component === 'Home');
+const isHomePage = computed(() => usePage().component === 'Home' || 'Text');
 
 
 const serverModal = ref<HTMLDialogElement>();
