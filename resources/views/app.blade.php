6aeb814e5b639bf145a41853cdaceb894929fb05
lempac <brunis.biksis@gmail.com>
2026-06-26 17:21:21 +0300

    Revert "Upgrade to Inertia v3"
    This reverts commit 1857ce3facaa797ff8c1dd4373dc345adc756067.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,27 +1,26 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="{{ auth()->user()?->theme->value ?? 'oxy' }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <x-inertia::head>
-            <title data-inertia>{{ config('app.name', 'Oxy') }}</title>
-        </x-inertia::head>
+    <title data-inertia>{{ config('app.name', 'Oxy') }}</title>
 
-        <!-- Reverb Config for Runtime -->
-        <meta name="reverb-app-key" content="{{ config('broadcasting.connections.reverb.key') }}">
-        <meta name="reverb-host" content="{{ config('broadcasting.connections.reverb.options.host') }}">
-        <meta name="reverb-port" content="{{ config('broadcasting.connections.reverb.options.port') }}">
-        <meta name="reverb-scheme" content="{{ config('broadcasting.connections.reverb.options.scheme') }}">
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Reverb Config for Runtime -->
+    <meta name="reverb-app-key" content="{{ config('broadcasting.connections.reverb.key') }}">
+    <meta name="reverb-host" content="{{ config('broadcasting.connections.reverb.options.host') }}">
+    <meta name="reverb-port" content="{{ config('broadcasting.connections.reverb.options.port') }}">
+    <meta name="reverb-scheme" content="{{ config('broadcasting.connections.reverb.options.scheme') }}">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
 
-        <!-- Scripts -->
-        @vite(['resources/js/app.ts'])
-    </head>
-    <body class="font-sans antialiased">
-        <x-inertia::app />
-        <div id="teleported"/>
-    </body>
+    <!-- Scripts -->
+    @vite(['resources/js/app.ts'])
+    <x-inertia::head/>
+</head>
+<body class="font-sans antialiased">
+<x-inertia::app/>
+<div id="teleported"/>
+</body>
 </html>
