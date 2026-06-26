80b55fa13f151bdbb52f79ccc0af7ede89fce47f
lempac <brunis.biksis@gmail.com>
2026-06-26 17:21:20 +0300

    Revert "Upgrade to Inertia v3"
    This reverts commit 810b69d77b6f5e3e7401523703c1440251cadc69.

--- resources/js/echo.ts
+++ resources/js/echo.ts
@@ -1,13 +1,11 @@
 import Echo from 'laravel-echo';
 
 import Pusher from 'pusher-js';
-if (typeof window !== 'undefined') {
-    window.Pusher = Pusher;
-}
+window.Pusher = Pusher;
 
-const meta = (name: string) => typeof document !== 'undefined' ? document.querySelector(`meta[name="${name}"]`)?.getAttribute('content') : null;
+const meta = (name: string) => document.querySelector(`meta[name="${name}"]`)?.getAttribute('content');
 
-const echo = typeof window !== 'undefined' ? new Echo({
+const echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY || meta('reverb-app-key'),
     wsHost: import.meta.env.VITE_REVERB_HOST || meta('reverb-host'),
@@ -15,6 +13,6 @@ const echo = typeof window !== 'undefined' ? new Echo({
     wssPort: import.meta.env.VITE_REVERB_PORT || meta('reverb-port') || 443,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME || meta('reverb-scheme') || 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
-}) : null;
+});
 
 export default echo;
--- resources/js/ssr.ts
+++ resources/js/ssr.ts
@@ -10,10 +10,8 @@ createServer((page) =>
         page,
         render: renderToString,
         title: (title) => `${title} - ${appName}`,
-        resolve: (name) => {
-            const pages = import.meta.glob<DefineComponent>('./Pages/**/*.vue');
-            return resolvePageComponent(`./Pages/${name}.vue`, pages);
-        },
+        // @ts-expect-error - Vite Inertia Plugin automatically resolves components
+        resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
         setup({ App, props, plugin }) {
             return createSSRApp({ render: () => h(App, props) })
                 .use(plugin);
