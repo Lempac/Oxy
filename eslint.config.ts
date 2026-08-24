03abac61a1c90429b7c1d856d4ba82b3d24dff98
lempac <brunis.biksis@gmail.com>
2026-08-25 00:23:54 +0300

    fix: replace deprecated tseslint.config with ESLint core defineConfig

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,10 +1,11 @@
 import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginVue from 'eslint-plugin-vue';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default defineConfig([
     { ignores: ['*.d.ts', '**/coverage', '**/dist', '**/vendor/**', '**/pb_hooks', '**/pb_migrations'] },
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
@@ -23,5 +24,5 @@ export default tseslint.config(
             },
         },
     },
-    eslintConfigPrettier
-);
+    eslintConfigPrettier,
+]);
